53bebb878e59defc4a115def5a46f0ed6a3b2eb3
Erik S. V. Jansson <caffeineviking@gmail.com>
2018-10-27 18:55:44 +0200

    Added default arguments to global include header.

--- include/vkhr/vkhr.hh
+++ include/vkhr/vkhr.hh
@@ -1,12 +1,14 @@
 #ifndef VKHR_VKHR_HH
 #define VKHR_VKHR_HH
 
+#include <vkhr/arguments.hh>
 #include <vkhr/arg_parser.hh>
 #include <vkhr/camera.hh>
 #include <vkhr/hair_style.hh>
 #include <vkhr/image.hh>
 #include <vkhr/input_map.hh>
 #include <vkhr/logger.hh>
+#include <vkhr/model.hh>
 #include <vkhr/paths.hh>
 #include <vkhr/rasterizer.hh>
 #include <vkhr/ray_tracer.hh>
