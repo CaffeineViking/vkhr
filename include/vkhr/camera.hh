08e91a6a819fb5d2c5797b3845cf490c64760bb0
Erik S. V. Jansson <caffeineviking@gmail.com>
2018-10-31 15:20:10 +0100

    Implemented abstract camera for Vulkan and Embree.

--- include/vkhr/camera.hh
+++ include/vkhr/camera.hh
@@ -1,7 +1,60 @@
 #ifndef VKHR_CAMERA_HH
 #define VKHR_CAMERA_HH
 
+#define GLM_FORCE_RADIANS
+#define GLM_ENABLE_EXPERIMENTAL
+
+#include <glm/glm.hpp>
+#include <glm/gtc/constants.hpp>
+#include <glm/gtc/quaternion.hpp>
+
 namespace vkhr {
+    class Camera final {
+    public:
+        Camera() = default;
+        Camera(const float field_of_view, const float aspect_ratio,
+               const float znear = 0.1, const float zfar = 1000.0);
+
+        void rotate(const glm::vec3& around_axis, const float angle);
+        void translate(const glm::vec3& translation);
+
+        float get_aspect_ratio() const;
+        void  set_aspect_ratio(const float aspect_ratio);
+        void  set_field_of_view(const float field_of_view);
+        float get_field_of_view() const;
+
+        const glm::vec3& get_position() const;
+        void set_position(const glm::vec3& position);
+        void set_look_at_point(const glm::vec3& look_at_point);
+        const glm::vec3& get_look_at_point() const;
+        void set_up_direction(const glm::vec3& up_direction);
+        const glm::vec3& get_up_direction() const;
+
+        glm::vec3 get_left_direction() const;
+        glm::vec3 get_forward_direction() const;
+
+        // TODO: convert to Embree-based camera.
+
+        const glm::mat4& get_view_matrix() const;
+        const glm::mat4& get_projection_matrix() const;
+
+    private:
+        void recalculate_projection_matrix();
+        void recalculate_view_matrix();
+
+        float near_distance { 0.3 };
+        float far_distance { 1000 };
+
+        float aspect_ratio { 16.0 / 9.0 };
+        float field_of_view { glm::quarter_pi<float>() };
+
+        glm::vec3 position { 0.0, 0.0, 4.0 };
+        glm::vec3 look_at_point { 0.0, 0.0, 0.0 };
+        glm::vec3 up_direction { 0.0, 1.0, 0.0 };
+
+        glm::mat4 view_matrix;
+        glm::mat4 projection_matrix;
+    };
 }
 
 #endif
--- src/vkhr/camera.cc
+++ src/vkhr/camera.cc
@@ -1,1 +1,96 @@
 #include <vkhr/camera.hh>
+
+#include <glm/gtx/rotate_vector.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+namespace vkhr {
+    Camera::Camera(const float field_of_view, const float aspect_ratio,
+                   const float znear, const float zfar)
+                  : near_distance { znear }, far_distance { zfar },
+                    aspect_ratio { aspect_ratio },
+                    field_of_view { field_of_view } {
+        recalculate_projection_matrix();
+        recalculate_view_matrix();
+    }
+
+    void Camera::rotate(const glm::vec3& around_axis, const float angle) {
+        auto look_at_vector = look_at_point - position;
+        glm::rotate(look_at_vector, angle, around_axis);
+        set_look_at_point(position + look_at_vector);
+    }
+
+    void Camera::translate(const glm::vec3& translation) {
+        set_position(this->position + translation);
+    }
+
+    void  Camera::set_aspect_ratio(const float aspect_ratio) {
+        this->aspect_ratio = aspect_ratio;
+        recalculate_projection_matrix();
+    }
+
+    float Camera::get_aspect_ratio() const {
+        return aspect_ratio;
+    }
+
+    void  Camera::set_field_of_view(const float field_of_view) {
+        this->field_of_view = field_of_view;
+        recalculate_projection_matrix();
+    }
+
+    float Camera::get_field_of_view() const {
+        return field_of_view;
+    }
+
+    const glm::vec3& Camera::get_position() const {
+        return position;
+    }
+
+    void Camera::set_position(const glm::vec3& position) {
+        this->position = position;
+        recalculate_view_matrix();
+    }
+
+    void Camera::set_look_at_point(const glm::vec3& look_at_point) {
+        this->look_at_point = look_at_point;
+        recalculate_view_matrix();
+    }
+
+    const glm::vec3& Camera::get_look_at_point() const {
+        return look_at_point;
+    }
+
+    void Camera::set_up_direction(const glm::vec3& up_direction) {
+        this->up_direction = glm::normalize(up_direction);
+        recalculate_view_matrix();
+    }
+
+    const glm::vec3& Camera::get_up_direction() const {
+        return up_direction;
+    }
+
+    const glm::mat4& Camera::get_view_matrix() const {
+        return view_matrix;
+    }
+
+    const glm::mat4& Camera::get_projection_matrix() const {
+        return projection_matrix;
+    }
+
+    glm::vec3 Camera::get_left_direction() const {
+        return glm::cross(get_forward_direction(),
+                          get_up_direction());
+    }
+
+    glm::vec3 Camera::get_forward_direction() const {
+        return glm::normalize(position - look_at_point);
+    }
+
+    void Camera::recalculate_projection_matrix() {
+        projection_matrix = glm::perspective(field_of_view, aspect_ratio,
+                                             near_distance, far_distance);
+    }
+
+    void Camera::recalculate_view_matrix() {
+        view_matrix = glm::lookAt(position, look_at_point, up_direction);
+    }
+}
