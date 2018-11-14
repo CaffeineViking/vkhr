c894207967a8d83aa96cb7c9420bb5871c05bdec
Erik S. V. Jansson <caffeineviking@gmail.com>
2018-11-14 11:39:29 +0100

    Commented out raytracer for now, going to add GUI.

--- src/main.cc
+++ src/main.cc
@@ -247,8 +247,6 @@ int main(int argc, char** argv) {
 
     window.show();
 
-    vkhr::Raytracer raytracer { camera, hair_style };
-
     while (window.is_open()) {
         if (input_map.just_pressed("quit")) {
             window.close();
