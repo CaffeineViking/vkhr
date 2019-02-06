cb80e7d9b1b9252283d4413e70c31b1ca01a8c04
Erik S. V. Jansson <caffeineviking@gmail.com>
2019-02-06 17:28:41 +0100

    Mostly complete PPLL implementation.
    - Node allocation
    - Writing
    - Finding current
    - Linking

--- share/shaders/transparency/ppll.glsl
+++ share/shaders/transparency/ppll.glsl
@@ -1,7 +1,7 @@
 #ifndef VKHR_PPLL_GLSL
 #define VKHR_PPLL_GLSL
 
-#define PPLL_NULL 0xffffffff
+#define PPLL_NULL_NODE 0xffffffff
 
 struct Node {
     vec4 color;
@@ -9,10 +9,33 @@ struct Node {
     uint next;
 };
 
-layout(binding = 6, r32ui)  uniform uimage2D heads;
+layout(binding = 6, r32ui)  uniform uimage2D ppll_heads;
 layout(binding = 7, std430) buffer LinkedList {
-    uint counter;
-    Node nodes[];
+    uint ppll_counter;
+    Node ppll_nodes[];
 };
+
+layout(constant_id = 1) const uint ppll_size = 0;
+
+uint ppll_next() {
+    uint next_node = atomicAdd(ppll_counter, 1u);
+    if (next_node > ppll_size)
+        return PPLL_NULL_NODE;
+    return next_node;
+}
+
+void ppll_put(vec4 color, float depth, uint node) {
+    ppll_nodes[node].color = color;
+    ppll_nodes[node].depth = depth;
+}
+
+uint ppll_head(ivec2 pixel) {
+    return imageLoad(ppll_heads, pixel).r;
+}
+
+uint ppll_link(uint old, /* to the */ uint new) {
+    return atomicExchange(ppll_nodes[old].next,
+                          new); // returns old.
+}
 
 #endif
