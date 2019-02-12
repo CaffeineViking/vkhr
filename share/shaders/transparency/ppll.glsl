7fe3d10d55f71a8cc7664c0508b9a3ce9c68e18b
Erik S. V. Jansson <caffeineviking@gmail.com>
2019-02-12 10:56:47 +0100

    Fixed PPLL implementation.

--- share/shaders/transparency/ppll.glsl
+++ share/shaders/transparency/ppll.glsl
@@ -1,40 +1,42 @@
 #ifndef VKHR_PPLL_GLSL
 #define VKHR_PPLL_GLSL
 
-#define PPLL_NULL_NODE 0xffffffff
+#define PPLL_EMPTY_NODE 0xffffffff
 
 struct Node {
     vec4 color;
     float depth;
-    uint next;
+    uint prev;
 };
 
-layout(binding = 6, r32ui)  uniform uimage2D ppll_heads;
+layout(binding = 6, r32ui) uniform uimage2D ppll_heads;
 layout(binding = 7, std430) buffer LinkedList {
     uint ppll_counter;
     uint ppll_size;
     Node ppll_nodes[];
 };
 
-uint ppll_next() {
+uint ppll_next_node() {
     uint next_node = atomicAdd(ppll_counter, 1u);
-    if (next_node > ppll_size)
-        return PPLL_NULL_NODE;
+    if (next_node >= ppll_size)
+        return PPLL_EMPTY_NODE;
+    ppll_nodes[next_node].prev = PPLL_EMPTY_NODE;
     return next_node;
 }
 
-void ppll_put(vec4 color, float depth, uint node) {
+void ppll_node_data(uint node, vec4 color, float depth, uint phead) {
     ppll_nodes[node].color = color;
     ppll_nodes[node].depth = depth;
+    ppll_nodes[node].prev  = phead;
 }
 
-uint ppll_head(ivec2 pixel) {
+uint ppll_find_head(ivec2 pixel) {
     return imageLoad(ppll_heads, pixel).r;
 }
 
-uint ppll_link(uint old, /* to the */ uint new) {
-    return atomicExchange(ppll_nodes[old].next,
-                          new); // returns old.
+uint ppll_link_node(ivec2 pixel, uint new_node) {
+    return imageAtomicExchange(ppll_heads, pixel,
+                               new_node);
 }
 
 #endif
