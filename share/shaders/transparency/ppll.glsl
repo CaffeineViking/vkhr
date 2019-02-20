1055476b16ef96787cf0814a7182f1f81b1c99c5
Erik S. V. Jansson <caffeineviking@gmail.com>
2019-02-20 16:05:25 +0100

    Added resolve pass to sort and blend fragments.

--- share/shaders/transparency/ppll.glsl
+++ share/shaders/transparency/ppll.glsl
@@ -1,7 +1,7 @@
 #ifndef VKHR_PPLL_GLSL
 #define VKHR_PPLL_GLSL
 
-#define PPLL_EMPTY_NODE 0xffffffff
+#define PPLL_NULL_NODE 0xffffffff
 
 struct Node {
     vec4 color;
@@ -20,15 +20,19 @@ layout(binding = 6, std430) buffer LinkedList {
 
 uint ppll_next_node() {
     uint next_node = atomicAdd(ppll_counter, 1u);
-    if (next_node >= ppll_size)
-        return PPLL_EMPTY_NODE;
-    ppll_nodes[next_node].prev = PPLL_EMPTY_NODE;
+    if (next_node >= 3686400) // TODO: ppll_size!
+        return PPLL_NULL_NODE;
+    ppll_nodes[next_node].prev = PPLL_NULL_NODE;
     return next_node;
 }
 
 void ppll_node_data(uint node, vec4 color, float depth) {
     ppll_nodes[node].color = color;
     ppll_nodes[node].depth = depth;
+}
+
+Node ppll_node(uint node) {
+    return ppll_nodes[node];
 }
 
 uint ppll_head_node(ivec2 pixel) {
